ccce81754fbb835a33397e8f477e6c3d0bc57fbe
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-10-02 18:04:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ if uploaded_files:
 
     # Visualizzare l'anteprima dei dati unici con opzione per UOMO/DONNA
     for index, row in unique_combinations.iterrows():
-        flag = st.radio(f"Articolo: {row['Articolo']}-{row['Colore']}", ('UOMO', 'DONNA'), key=index)
+        flag = st.radio(f"{row['Articolo']}-{row['Colore']}", ('UOMO', 'DONNA'), key=index)
         selections[(row['Articolo'], row['Colore'])] = flag
 
     if st.button("Elabora File"):
